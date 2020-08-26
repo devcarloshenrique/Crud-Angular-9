ef5c8b67368d685aab8d2cbdda05333b69936e1b
devcarloshenrique <devcarloshenrique@gmail.com>
2020-08-26 09:48:00 -0300

    feat: component nav

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,9 +8,10 @@ import { HeaderComponent } from './components/template/header/header.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FooterComponent } from './components/template/footer/footer.component';
+import { NavComponent } from './components/template/nav/nav.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, NavComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
