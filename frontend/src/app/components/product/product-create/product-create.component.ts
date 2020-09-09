ba064d619b12d75e38445f5142e09c72107cce04
devcarloshenrique <devcarloshenrique@gmail.com>
2020-09-09 16:21:41 -0300

    refactor: button to create and cancel new product

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from './../product.service';
+
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-create',
@@ -6,7 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-create.component.css'],
 })
 export class ProductCreateComponent implements OnInit {
-  constructor() {}
+  constructor(private productService: ProductService, private router: Router) {}
 
   ngOnInit(): void {}
+
+  createProduct(): void {
+    this.productService.showOnConsole('Produto Criado !');
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products']);
+  }
 }
